158243a44245fefb43306b8d436180f7831dd291
Darya Podskrebalina <2012dr2012@gmail.com>
2023-06-18 01:52:08 +0600

    feat: install vue and tailwind css

---
+++ pusk-vue/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Главная</RouterLink>
+        <RouterLink to="/jobs">Вакансии</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
